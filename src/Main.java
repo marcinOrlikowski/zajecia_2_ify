c3396509dfe7722d05927b95a26daf86a6fb138e
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-13 15:07:15 +0200

    master - changed base main and gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 }
