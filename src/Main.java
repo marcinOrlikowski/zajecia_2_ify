51a999d82edf0156f28db0703cfb4e53dec08350
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-14 12:39:18 +0200

    TASK-0011 added leapYear method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 public class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
+        //zadanie 11
+        leapYear();
+        /**
+         * Output:
+         * Podaj rok:
+         * 1992
+         * 1992 jest rokiem przestępnym
+         */
+    }
 
+    private static void leapYear() {
+        System.out.println("Podaj rok:");
+        int year = SCANNER.nextInt();
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " jest rokiem przestępnym");
+        } else
+            System.out.println(year + " nie jest rokiem przestępnym");
     }
 }
