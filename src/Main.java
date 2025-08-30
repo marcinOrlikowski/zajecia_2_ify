38841397314557a192e714e629f6b5df6c943066
marcinOrlikowski <marcin.orlikowski95@gmail.com>
2025-08-30 15:28:12 +0200

    feature/TASK-0003 code formatting

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
         //zadanie 3
         ageCategorization();
@@ -19,28 +20,23 @@ public class Main {
 
     private static void ageCategorization() {
         int age = readAge();
-        if (age < 12)
-            System.out.println("Dziecko");
-        else if (age < 18)
-            System.out.println("Nastolatek");
-        else if (age < 65)
-            System.out.println("Dorosły");
-        else
-            System.out.println("Senior");
+        if (age < 12) System.out.println("Dziecko");
+        else if (age < 18) System.out.println("Nastolatek");
+        else if (age < 65) System.out.println("Dorosły");
+        else System.out.println("Senior");
     }
 
-    private static int readAge(){
+    private static int readAge() {
         int age = 0;
         boolean isAgeCorrect = false;
         do {
             try {
                 System.out.println("Podaj wiek:");
                 age = SCANNER.nextInt();
-                if (age <= 0 || age > 150)
-                    System.err.println("Podano nieprawidłowy wiek");
+                if (age <= 0 || age > 150) System.err.println("Podano nieprawidłowy wiek");
                 else isAgeCorrect = true;
             } catch (Exception e) {
-                System.err.println("Nieprawidłowy format - podaj liczbe całkowitą");;
+                System.err.println("Nieprawidłowy format - podaj liczbe całkowitą");
             } finally {
                 SCANNER.nextLine();
             }
